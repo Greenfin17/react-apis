0a8b587d9358b8a979ffd18fbc99af3c6236bcdc
Greenfin17 <jmaple@yahoo.com>
2021-04-24 01:21:35 -0500

    adding styling

--- src/App/index.js
+++ src/App/index.js
@@ -1,54 +1,48 @@
-import { React, useEffect, useState } from 'react';
+import { React, useState } from 'react';
 import './App.scss';
 // import NavBar from '../components/Navbar';
 // import JokeGenerator from '../components/JokeGenerator';
 import { getJoke } from '../helpers/data/jokesData';
+import jokeImg from '../assets/jokegen.png';
 
 function App() {
   const [showJoke, setShowJoke] = useState(false);
-  const [showInitialButton, setShowInitialButton] = useState(true);
-  const [showPunchLine, setShowPunchLine] = useState(false);
+  const [showPunchLine, setShowPunchLine] = useState(true);
   const [jokeSetup, setJokeSetup] = useState('');
   const [jokePunchLine, setPunchLine] = useState('');
+  const [visiblePunchLine, setVisiblePunchLine] = useState('');
+  const [buttonText, setButtonText] = useState('Get a joke');
 
   const getJokeClick = () => {
     if (showPunchLine) {
       getJoke().then((joke) => {
+        setShowJoke(true);
         setJokeSetup(joke.setup);
         setPunchLine(joke.punchline);
-        setShowInitialButton(false);
+        setVisiblePunchLine('');
         setShowPunchLine(false);
+        setButtonText('Show punchline');
       });
     } else if (showJoke) {
       setShowPunchLine(true);
+      setVisiblePunchLine(jokePunchLine);
+      setButtonText('Get a joke');
     }
   };
-
-  const initialClick = () => {
-    setShowInitialButton(false);
-    setShowJoke(true);
-  };
-
-  useEffect(() => {
-    getJoke()
-      .then((joke) => {
-        setJokeSetup(joke.setup);
-        setPunchLine(joke.punchline);
-      });
-  }, []);
 
   return (
     <div className='App'>
       <h1>Joke Generator</h1>
-      { showJoke && <p className='joke-setup'>{jokeSetup}</p> }
-      { showPunchLine && <p className='joke-punchline'>{jokePunchLine}</p> }
-
-      { showInitialButton && <button onClick={initialClick} className='initalJoke'>Get a joke</button> }
-
-      { !showInitialButton && <button onClick={getJokeClick}>
-          { showPunchLine ? 'Get a new Joke' : 'Show punchline' }
-        </button>
-      }
+      <div className='jokes'>
+        <div className='card'>
+          <img src={jokeImg} className='card-img-top' alt="Joke Generator" />
+          <div className='card-body'>
+            { showJoke && <p className='card-text setup'>{jokeSetup}</p> }
+            <p className='card-text punchline'>{visiblePunchLine}</p>
+            <button onClick={getJokeClick} className='btn btn-dark'>{buttonText}</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
