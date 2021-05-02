bc8ba51c19bc082ad98cce9e1eca1ad78b3e31ba
Greenfin17 <jmaple@yahoo.com>
2021-05-01 23:16:30 -0500

    Adding title for Characters page

--- src/components/LordRings.js
+++ src/components/LordRings.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import LordNav from './navigation/LordNav';
 import RingCard from './cards/RingCard';
 import { getCharactersWithQuotes } from '../helpers/data/ringsData';
 
@@ -9,18 +8,12 @@ function LordRings() {
   useEffect(() => {
     getCharactersWithQuotes().then((responseArr) => {
       setCharactersWithQuotes(responseArr);
-      for (let i = 0; i < 100; i += 1) {
-        if (responseArr[i].quotes.length) {
-          console.warn(responseArr[i].quotes[0].dialog);
-        }
-      }
     });
   }, []);
 
   return (
     <div className='lord-rings-page'>
-      <LordNav
-        />
+      <h1>Characters from Lord of the Rings</h1>
       <div className='lord-rings-container d-flex flex-wrap justify-content-around'>
       {charactersWithQuotes.map((character, key) => (
         <RingCard
