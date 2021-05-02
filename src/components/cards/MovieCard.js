bd08208c2838d9c99b50c8d667a19d85bb38c007
Greenfin17 <jmaple@yahoo.com>
2021-05-02 13:59:07 -0500

    adding movie card

--- src/components/cards/MovieCard.js
+++ src/components/cards/MovieCard.js
@@ -1,1 +1,37 @@
 // MovieCard.js
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MovieCard = ({
+  Budget,
+  Revenue,
+  Nominations,
+  Wins,
+  RTScore
+}) => (
+  <Card>
+    <CardBody>
+      <CardTitle tag="h5">Card title</CardTitle>
+      <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+      <ul>
+        <li>Budget: {Budget}</li>
+        <li>Revenue: {Revenue}</li>
+        <li>Academy Nominations:  {Nominations}</li>
+        <li>Academy Wins: {Wins}</li>
+        <li>Rotten Tomatoe Score: {RTScore}</li>
+      </ul>
+      <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+      <Button>Button</Button>
+    </CardBody>
+  </Card>
+);
+
+MovieCard.propTypes = {
+  Budget: PropTypes.number,
+  Revenue: PropTypes.number,
+  Nominations: PropTypes.number,
+  Wins: PropTypes.number,
+  RTScore: PropTypes.number
+};
+
+export default MovieCard;
